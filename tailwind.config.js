bd553ab5fe2ba8f16e673242f8292c2f81f68928
sayus2884 <joniah2884@gmail.com>
2021-10-28 01:14:46 -0700

    style: add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,51 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "code-image": "url('/images/code.jpg')",
+      },
+      colors: {
+        // Configure your color palette here
+        midnight: "#1C1C1C",
+        moonlight: "#FFCB6B",
+      },
+      spacing: {
+        8: "8px",
+        10: "10px",
+        12: "12px",
+        22: "22px",
+        24: "24px",
+        25: "25px",
+        35: "35px",
+        45: "45px",
+        55: "55px",
+        70: "70px",
+        80: "80px",
+        90: "90px",
+        100: "100px",
+        120: "120px",
+        135: "135px",
+        300: "300px",
+        360: "360px",
+        800: "800px",
+      },
+      maxWidth: {
+        1320: "1320px",
+      },
+      fontSize: {
+        title: "64px",
+        20: "20px",
+        22: "22px",
+        24: "24px",
+        32: "32px",
+        36: "36px",
+        130: "130px",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
