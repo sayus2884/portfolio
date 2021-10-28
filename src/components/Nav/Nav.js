1e8b8c8a50b07f718daefa89ae6f1b225a271978
sayus2884 <joniah2884@gmail.com>
2021-10-27 21:42:17 -0700

    style: add nav style

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,35 @@
 import { Container } from "./Nav.styles";
 
+const navItems = [
+  { name: "Works", route: "#works" },
+  { name: "WhatIOffer", route: "#offer" },
+  { name: "Tools", route: "#tools" },
+];
+
 function Nav() {
   return (
-    <Container>
-      <div>add nav stuff here</div>
-    </Container>
+    <header className="flex fixed h-16 bg-midnight w-full text-white items-center justify-between px-55">
+      <div>Jonacius Villamor</div>
+      <nav>
+        <ul className="flex items-center gap-10">
+          <li className="border-r pr-10">
+            <a className="cursor-pointer">Dark</a>
+          </li>
+
+          {navItems.map((item, i) => (
+            <li key={i}>
+              <a className="cursor-pointer">{item.name}</a>
+            </li>
+          ))}
+
+          <li>
+            <button className="text-moonlight border border-moonlight px-10 py-8 rounded">
+              <a className="cursor-pointer">Get In Touch</a>
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
