0a826df19bd55a6ea3cf1906685a4da3194896e1
sayus2884 <joniah2884@gmail.com>
2021-10-29 20:06:39 -0700

    refactor: change default

--- src/hooks/use-work-modal.js
+++ src/hooks/use-work-modal.js
@@ -1,7 +1,7 @@
 import { useCallback, useState } from "react";
 
 const UseWorkModal = () => {
-  const [showWorkModal, setShowWorkModal] = useState(true);
+  const [showWorkModal, setShowWorkModal] = useState(false);
 
   const openWorkModal = useCallback(() => setShowWorkModal(true), []);
   const closeWorkModal = useCallback(() => setShowWorkModal(false), []);
--- src/hooks/use-work.js
+++ src/hooks/use-work.js
@@ -1,17 +1,7 @@
 import { useCallback, useState } from "react";
 
 const UseWork = () => {
-  const [selectedWork, setSelectedWork] = useState({
-    src: "/images/face1.jpg",
-    title: `Wonderful 010`,
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
-    technologies: [
-      { name: "Next.js", url: "https://nextjs.org/" },
-      { name: "styled-components", url: "https://styled-components.com/" },
-      { name: "Tailwind CSS", url: "https://tailwindcss.com/" },
-    ],
-  });
+  const [selectedWork, setSelectedWork] = useState({});
 
   return { selectedWork, setSelectedWork };
 };
