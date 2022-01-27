388fad5262d444580058e99c97687b7ffef8d681
sayus2884 <joniah2884@gmail.com>
2022-01-27 02:23:03 -0800

    update: add link to myoshis.ch

--- src/sections/Construction/Construction.js
+++ src/sections/Construction/Construction.js
@@ -5,7 +5,7 @@ function Construction(props) {
     <section
       {...props}
       className="justify-center items-center pt-16 h-screen flex relative overflow-hidden">
-      <div className="absolute z-10 top-250">
+      <div className="absolute z-10 top-225">
         <div className="flex relative items-center justify-center h-500 w-500">
           <img className="absolute h-max w-max" src="/images/logo.svg" />
           <img className="absolute h-max w-max animate-spin-rays" src="/images/rays.svg" />
@@ -20,15 +20,18 @@ function Construction(props) {
         </div>
       </div>
 
-      <div className="flex flex-col gap-35 items-center absolute top-0 bg-blackberry m-25 px-35 pt-22 pb-22 rounded-md">
+      <div className="flex flex-col gap-30 items-center absolute top-0 bg-blackberry m-25 px-35 pt-22 pb-22 rounded-md">
         <p className="text-plum text-12 z-50 w-500 text-center">
           My portfolio is currently under construction. In the mean time, feel free to look into one
           my works and dive into the world of Vtubers!
         </p>
 
-        <button className="text-gum border border-gum rounded-md p-10 hover:bg-plum/50">
+        <a
+          className="text-gum border border-gum rounded-md p-10 hover:bg-plum hover:text-white"
+          href="https://myoshis.ch"
+          target="_blank">
           Into the rabbit hole
-        </button>
+        </a>
       </div>
     </section>
   );
