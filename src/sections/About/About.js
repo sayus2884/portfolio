1bcddc031d3611a9805ad9753b3da7d513f2b967
sayus2884 <joniah2884@gmail.com>
2022-02-09 22:16:29 -0800

    update: justify about description

--- src/sections/About/About.js
+++ src/sections/About/About.js
@@ -13,7 +13,7 @@ function About({ children, className, ...props }) {
       <div className="relative flex flex-col gap-30 items-center px-45 h-full overflow-auto">
         <img src="/images/profile.png" className="h-200 rounded-full border-2 border-plum" />
 
-        <p className="text-18 text-center px-55">
+        <p className="text-18 text-justify px-55">
           I am a fullstack developer based in British Columbia, Canada. I like to build web apps
           which are very useful and convenient. I do enjoy drawing which taught me some pretty
           useful design and user experience techniques that I incorporate into my projects. I love
