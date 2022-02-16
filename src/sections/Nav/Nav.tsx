9e387ff471d56e1fa6d0661b2261fb92f70e90f9
sayus2884 <joniah2884@gmail.com>
2022-02-15 23:23:02 -0800

    update: add nav media style

--- src/sections/Nav/Nav.tsx
+++ src/sections/Nav/Nav.tsx
@@ -6,7 +6,7 @@ import Dropdown from "../../components/Dropdown/Dropdown";
 
 import NavigationContext from "../../contexts/ProjectNavigationContext";
 
-const [PROJECTS, ABOUT, CONTACT] = [0, 1, 2];
+const [PROJECTS, ABOUT, CONTACT] = ["/works", "/about", "/contact"];
 
 function Nav({ className, ...props }) {
   const router = useRouter();
@@ -17,8 +17,14 @@ function Nav({ className, ...props }) {
     { name: "Contact", route: "/contact" },
   ];
 
+  const handleNavigation = (route) => {
+    setActive(route);
+    router.push(route);
+  };
+
   return (
-    <section className={`${className} flex flex-row  justify-between items-center z-50`}>
+    <section
+      className={`${className} flex flex-row justify-between items-center z-50 md:px-20 lg:px-40 xl:px-60`}>
       <div className="flex space-x-8">
         <Link href={"/"}>
           <h1 className="font-header cursor-pointer">Jonacius Villamor</h1>
@@ -26,17 +32,17 @@ function Nav({ className, ...props }) {
       </div>
 
       <nav>
-        <div className="hidden md:block lg:pr-20">
-          <ul className="flex gap-15 justify justify-end font-bold">
+        <div className="hidden md:block">
+          <ul className="flex gap-30 justify justify-end font-bold">
             {routes.map(({ name, route }, i) => (
               <li
                 key={i}
-                className={`text-20 hover:text-white hover:opacity-50 ${
-                  active === PROJECTS && "text-white"
+                className={`text-18 ${
+                  active === route ? "text-plum" : "hover:text-white hover:opacity-50"
                 }`}>
-                <Link href={route}>
-                  <button>{name}</button>
-                </Link>
+                <button className="tracking-wide" onClick={() => handleNavigation(route)}>
+                  {name}
+                </button>
               </li>
             ))}
           </ul>
